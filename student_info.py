a675bef9ce5e801f4314090292d11e57262cf53c
sindhugoutham <sindhubatchu.07@gmail.com>
2026-07-08 22:32:22 +0530

    Day 1 Python basics

---
+++ student_info.py
@@ -0,0 +1,9 @@
+name=input('what is your name! ')
+age=input('what is your age ')
+language=input('what is your fav language ')
+company=input('what is your dream company ')
+
+print("hello " + name)
+print("you are " + age + " years old")
+print("your fav language is " + language )
+print("you want to work at " + company )
