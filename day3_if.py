171c9e9b560a031f1b1ee92451744631fb54105d
sindhugoutham <sindhubatchu.07@gmail.com>
2026-07-15 21:40:42 +0530

    day 3:operators and calculator

---
+++ day3_if.py
@@ -0,0 +1,8 @@
+first_num = int(input("Enter the first number: "))
+second_num = int(input("Enter the second number: "))
+if first_num > second_num:
+    print("First number is greater than second number")
+elif first_num < second_num:
+    print("First number is less than second number")
+else:
+    print("Both numbers are equal")
---
+++ simple_calculator.py
@@ -0,0 +1,16 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+sum=a+b
+differenc=a-b
+product=a*b
+
+print(sum)
+print(differenc)
+print(product)
+
+if b == 0:
+    print("divison by zero is not possible")
+else:
+    print("quonitent")
+
+
