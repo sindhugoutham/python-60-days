ba72a510669b945dbeba1540b0a04cdf59fc8935
sindhugoutham <sindhubatchu.07@gmail.com>
2026-07-16 13:47:42 +0530

    add shopping cart

---
+++ shopping_cart.py
@@ -0,0 +1,8 @@
+product=(input("enter name "))
+price=int(input("enter amount"))
+quantity=int(input("enter quantity"))
+total_amount=price*quantity
+print(product)
+print(price)
+print(quantity)
+print(total_amount)
